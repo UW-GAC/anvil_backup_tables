387b2de9d48fcc8a21e60951f3c42215d2e523d6
Adrienne Stilp <amstilp@uw.edu>
2023-08-28 16:34:15 -0700

    Remove tempdir and just copy files using *.tsv

--- anvil_backup_tables.R
+++ anvil_backup_tables.R
@@ -11,23 +11,20 @@ argp <- arg_parser("anvil_backup_tables") %>%
 argv <- parse_args(argp)
 print(argv)
 
-# # Create a tempdir in which to store the data tables.
-tmpdir <- tempdir()
-
 # Loop over tables and write a tsv.
 tables <- avtables(namespace=argv$workspace_namespace, name=argv$workspace_name)$table
 for (t in tables) {
     message(sprintf("Backing up %s", t))
     table_data <- avtable(t, namespace=argv$workspace_namespace, name=argv$workspace_name)
-    outfile <- file.path(tmpdir, sprintf("%s.tsv", table))
-    # outfile <- sprintf("%s.tsv", t)
+    # outfile <- file.path(tmpdir, sprintf("%s.tsv", table))
+    outfile <- sprintf("%s.tsv", t)
     write_tsv(table_data, outfile)
 }
 
 # Print the files out (for testing?).
-list.files(tmpdir)
+list.files()
 
 # # Copy the output to the final destination.
-gsutil_cp(file.path(tmpdir, "*"), argv$output_bucket_path)
+gsutil_cp("*.tsv", argv$output_bucket_path)
 
 message("Done!")
