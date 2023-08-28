7cedeebfcd4d37e6aba3bb75812091084cb036ad
Adrienne Stilp <amstilp@uw.edu>
2023-08-28 11:44:37 -0700

    Fix argparser function name in R script

--- anvil_backup_tables.R
+++ anvil_backup_tables.R
@@ -1,7 +1,7 @@
 library(argparser)
 library(tidyverse)
 
-argp <- argparser("anvil_backup_tables") %>%
+argp <- arg_parser("anvil_backup_tables") %>%
     add_argument("--name", help="Name to say hello to")
 
 sprintf("Hello world!")
