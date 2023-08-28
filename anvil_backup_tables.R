0d3873181b83f100d9153dfe629f706d7bcc84d0
Adrienne Stilp <amstilp@uw.edu>
2023-08-28 16:30:20 -0700

    Copy output files to the workspace bucket path

--- anvil_backup_tables.R
+++ anvil_backup_tables.R
@@ -12,23 +12,22 @@ argv <- parse_args(argp)
 print(argv)
 
 # # Create a tempdir in which to store the data tables.
-# tmpdir <- tempdir()
+tmpdir <- tempdir()
 
 # Loop over tables and write a tsv.
 tables <- avtables(namespace=argv$workspace_namespace, name=argv$workspace_name)$table
 for (t in tables) {
     message(sprintf("Backing up %s", t))
     table_data <- avtable(t, namespace=argv$workspace_namespace, name=argv$workspace_name)
-    # outfile <- file.path(tmpdir, sprintf("%s.tsv", table))
-    outfile <- sprintf("%s.tsv", t)
+    outfile <- file.path(tmpdir, sprintf("%s.tsv", table))
+    # outfile <- sprintf("%s.tsv", t)
     write_tsv(table_data, outfile)
 }
 
 # Print the files out (for testing?).
-# list.files(tmpdir)
-list.files()
+list.files(tmpdir)
 
 # # Copy the output to the final destination.
-# gsutil_cp(file.path(tmpdir, "*"), bucket_path)
+gsutil_cp(file.path(tmpdir, "*"), bucket_path)
 
 message("Done!")
