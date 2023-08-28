48e5c772f533fb65f27af3e15ce80b544d9a23f2
Adrienne Stilp <amstilp@uw.edu>
2023-08-28 16:54:54 -0700

    Create json file of table data for use in validation scripts

--- anvil_backup_tables.R
+++ anvil_backup_tables.R
@@ -1,6 +1,7 @@
 library(argparser)
 library(magrittr)
 library(readr)
+library(jsonlite)
 library(AnVIL)
 sessionInfo()
 
@@ -12,6 +13,7 @@ argv <- parse_args(argp)
 print(argv)
 
 # Loop over tables and write a tsv.
+table_json <- list()
 tables <- avtables(namespace=argv$workspace_namespace, name=argv$workspace_name)$table
 for (t in tables) {
     message(sprintf("Backing up %s", t))
@@ -19,12 +21,18 @@ for (t in tables) {
     # outfile <- file.path(tmpdir, sprintf("%s.tsv", table))
     outfile <- sprintf("%s.tsv", t)
     write_tsv(table_data, outfile)
+    table_json[[t]] <- file.path(argv$output_bucket_path, outfile)
 }
 
 # Print the files out (for testing?).
 list.files()
 
-# # Copy the output to the final destination.
+# Copy the output to the final destination.
 gsutil_cp("*.tsv", argv$output_bucket_path)
+
+# Save the json file with table inputs.
+outfile <- "table_files.json"
+writeLines(toJSON(table_json), outfile)
+gsutil_cp(outfile, argv$output_bucket_path)
 
 message("Done!")
